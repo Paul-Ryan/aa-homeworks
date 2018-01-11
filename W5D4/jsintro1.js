8f2c93c27a327d19e9316c6dc6dcc0871c5ce9f8
Paul <paul.ryan.iii@gmail.com>
2018-01-10 21:45:27 -0800

    Finish js intro 1

--- W5D4/jsintro1.js
+++ W5D4/jsintro1.js
@@ -9,4 +9,91 @@ function madLib(verb, adjective, noun) {
   return 'We shall ${verb.toUpperCase()} the ${adjective.toUpperCase()} ${noun.toUpperCase()}';
 }
 
-madLib('make', 'best', 'guac');
+// function madLib(verb, adj, noun) {
+//   return `We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}.`
+// }
+
+// console.log(madLib('make', 'best', 'guac'));
+
+
+function isSubstring(str1, str2) {
+  return str1.includes(str2);
+}
+//
+// console.log(isSubstring("time to program", "time"));
+// console.log(isSubstring("Jump for joy", "joys"));
+
+
+
+
+// fizzBuzz
+// 3 and 5 are magic numbers.
+//
+// Define a function fizzBuzz(array) that takes an array and returns a new array of every number in the array that is divisible by either 3 or 5, but not both.
+
+function fizzBuzz(arr) {
+  var fizzArr = [];
+
+  for (var i = 0; i < arr.length; i++ ) {
+    let num = arr[i];
+    if (num % 3 === 0 && num % 5 === 0) {
+      continue;
+    }
+    if (arr[i] % 3 === 0 || arr[i] % 5 === 0) {
+      fizzArr.push(num);
+    }
+  }
+
+  return fizzArr;
+}
+
+// console.log(fizzBuzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 18]));
+// console.log(fizzBuzz([15, 18]));
+
+
+// isPrime
+// Define a function isPrime(number) that returns true if number is prime. Otherwise, false. Assume number is a positive integer.
+//
+
+function isPrime(num) {
+  if (num < 2) { return false; }
+
+  for (var i = 2; i < num; i++) {
+    if (num % i === 0) { return false; }
+  }
+
+  return true;
+}
+
+console.log(isPrime(2));
+console.log(isPrime(10));
+console.log(isPrime(15485863));
+console.log(isPrime(3548563));
+// false
+// sumOfNPrimes
+// Using firstNPrimes, write a function sumOfNPrimes(n) that returns the sum of the first n prime numbers. Hint: use isPrime as a helper method.
+
+function sumOfNPrimes(n) {
+  var numPrimes = 0;
+  var sum = 0;
+  var i = 2;
+
+  while (numPrimes < n) {
+    if (isPrime(i)) {
+      numPrimes ++;
+      sum += i;
+    }
+
+    i++;
+  }
+  return sum;
+}
+
+console.log(sumOfNPrimes(0));
+// 0
+
+console.log(sumOfNPrimes(1));
+// 2
+
+console.log(sumOfNPrimes(4));
+// 17
