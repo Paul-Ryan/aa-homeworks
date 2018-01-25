3790fe783106dd8877efc500796f2eeb398e71c3
Paul <paul.ryan.iii@gmail.com>
2018-01-24 21:16:55 -0800

    Rainbow Routed

---
+++ W7D4/rainbow_routes/components/red.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Link, NavLink } from 'react-router-dom';
+import Orange from './orange';
+import Yellow from './yellow';
+
+class Red extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2 className="red"></h2>
+        <Link to='/red' >Red only</Link>
+        <Link to='red/orange' >Red Orange </Link>
+        <Link to='red/yellow' >Red yellow </Link>
+
+        {<Route path="/red/orange" component={Orange} />}
+        {<Route path="/red/yellow" component={Yellow} />}
+
+      </div>
+    );
+  }
+};
+
+export default Red;
